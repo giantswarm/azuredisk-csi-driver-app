a93a52f8eab03ae1710b750415434ef514850946
Christian Bianchi <whites11@gmail.com>
2022-03-15 09:49:32 +0100

    disable test as it can't work outside azure

--- integration/templates/values.go
+++
@@ -1,9 +0,0 @@
-//go:build k8srequired
-// +build k8srequired
-
-package templates
-
-// AzurediskCsiValues values required by azuredisk-csi-driver-app.
-const AzurediskCsiValues = `
-name: azuredisk-csi-test
-`
